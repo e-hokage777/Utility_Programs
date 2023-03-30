7e753e91dc599732ed94bc188326f2b83a06908c
e-hokage777 <e.hokage777@gmail.com>
2023-03-30 22:52:21 +0000

    updated medium_scraper.py

--- python/medium_scraper/medium_scraper.py
+++ python/medium_scraper/medium_scraper.py
@@ -1,14 +1,15 @@
 ## importing libraries
-import requests
+from urllib.request import Request, urlopen
 from bs4 import BeautifulSoup
-import os
-
-# https://towardsdatascience.com/generalised-additive-models-6dfbedf1350a
 
 def write_page(url, save_name):
+    ## creating a request object
+    req = Request(url = url,
+                  headers = {"User-agent":"Mozilla/5.0"})
     ## getting the page
     try:
-        page = requests.get(url).content       
+        # page = requests.get(url).content
+        page = urlopen(req).read()
     except:
         print("Error getting page. Please check your url")
         return
@@ -18,24 +19,12 @@ def write_page(url, save_name):
     with open(f"scraped_files/{save_name}.html", "w", encoding = "utf-8") as file:
         file.write(soup.prettify())
 
-def check_n_create_dir():
-    print("Checking if 'scraped_files' folder already exists...")
-    if not os.path.isdir("./scraped_files"):
-        print("Folder does not exist")
-        print("Creating folder .... ")
-
-        os.mkdir("./scraped_files")
-    else:
-        print("'scraped_files folder already exists'")
-
 
 if(__name__ == "__main__"):
     print("Welcome, ready to scrape some medium?")
     url = input("Page URL >> ")
     save_name = input("Page savename >> ")
 
-    check_n_create_dir()
-
-    print("Working on page ..... ")
+    print("Working ..... ")
     write_page(url, save_name)
     print("Done !!")
